c82069549e286ec450f3de3df5bc4ab9727fa81c
Heurtault Aubin <aub.heurtault@gmail.com>
2022-10-01 14:08:03 +0200

    setup deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+    router: {
+      base: '/TodoAppNuxt3/'
+    },
     modules: [
         'cookie-universal-nuxt'
     ]
