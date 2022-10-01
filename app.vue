cf17d177b6e2fe697a94e9a85f822ef5a3b82e8a
Heurtault Aubin <aub.heurtault@gmail.com>
2022-10-01 13:59:33 +0200

    add script todo'

--- app.vue
+++ app.vue
@@ -16,3 +16,49 @@
     <h4 v-if="todos.length === 0">Empty list.</h4>
   </div>
 </template>
+<script setup>
+  const newTodo = ref('')
+  const defaultData = [
+    {
+      done: true,
+      content: 'Write a blog post'
+    },
+    {
+      done: false,
+      content: 'Listen a podcast'
+    }
+  ]
+  const { $cookies } = useNuxtApp()
+  const todosData = $cookies.get('todos') || defaultData
+  const todos = ref(todosData)
+  function addTodo() {
+    if (newTodo.value) {
+      todos.value.push({
+        done: false,
+        content: newTodo.value
+      })
+      newTodo.value = ''
+    }
+    saveData()
+  }
+  function doneTodo(todo) {
+    todo.done = !todo.done
+    saveData()
+  }
+  function removeTodo(index) {
+    todos.value.splice(index, 1)
+    saveData()
+  }
+  function saveData() {
+    $cookies.set('todos', todos.value)
+  }
+  useHead({
+    title: 'ToDo App',
+    meta: [
+      {
+        name: 'description',
+        content: 'Nuxt 3 ToDo App with Composition API'
+      }
+    ]
+  })
+  </script>
